a875d2d079e361754435783352a42e09d8ad4465
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-10-04 00:07:45 -0500

    MVP + all stretch goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+
+
 axios
     .get("https://api.github.com/users/viewgo")
     .then(response => {
-        // console.log(response);
+        console.log(response);
         const cards = document.querySelector(".cards");
-        cards.appendChild(CardMaker(response.data));
+        
+        cards.appendChild(CardMaker(response.data, true));
         return response.data.followers_url;
     })
 
@@ -18,8 +21,8 @@ axios
                 axios
                   .get(`${element.url}`)
                   .then(response => {
-                    console.log(response);
-                    cards.appendChild(CardMaker(response.data));
+                    // console.log(response);
+                    cards.appendChild(CardMaker(response.data, true));
                 });
             });
         });
@@ -28,9 +31,23 @@ axios
         console.log("The data was not returned", error);
     });
 
+    /*
+    <cards>
+        <card>
+            <holder>
+                <img>
+                <cardInfo>
+            <span>
+            <calendar>
 
-function CardMaker(data) {
-    const card = document.createElement("div"),
+            */
+
+function CardMaker(data, useCalendar) {
+    const 
+        card = document.createElement("div"),
+        holderDiv = document.createElement("div"),
+        button = document.createElement("div"),
+        calendarDiv = document.createElement("div"),
         img = document.createElement("img"),
         cardInfo = document.createElement("div"),
         name = document.createElement("h3"),
@@ -43,12 +60,20 @@ function CardMaker(data) {
         bio = document.createElement("p");
 
     card.classList.add("card");
+    holderDiv.classList.add("holder");
+    button.classList.add("expand-button");
+    calendarDiv.classList.add("calendar");
+    calendarDiv.classList.add("calendar-hidden");
     cardInfo.classList.add("card-info");
     name.classList.add("name");
     username.classList.add("username");
 
-    card.appendChild(img);
-    card.appendChild(cardInfo);
+    card.appendChild(holderDiv);
+    card.appendChild(button);
+    card.appendChild(calendarDiv);
+
+    holderDiv.appendChild(img);
+    holderDiv.appendChild(cardInfo);
 
     cardInfo.appendChild(name);
     cardInfo.appendChild(username);
@@ -58,19 +83,48 @@ function CardMaker(data) {
     cardInfo.appendChild(following);
     cardInfo.appendChild(bio);
 
+    const buttonText1 = "Show Contributions";
+    const buttonText2 = "Hide Contributions";
+    button.textContent = buttonText1;
+
     img.src = data.avatar_url;
-    name.textContent = data.name;
+
+    (data.login !== null) ? name.textContent = data.name : name.style.display = "none";
     username.textContent = data.login;
-    location.textContent = "Location: " + data.location;
+    (data.location !== null) ? location.textContent = "Location: " + data.location : location.style.display = "none";
     profile.textContent = "Profile: ";
     a.href = data.html_url;
     a.target = "_blank";
     a.textContent = data.html_url;
     followers.textContent = "Followers: " + data.followers;
     following.textContent = "Following: " + data.following;
-    bio.textContent = "Bio: " + data.bio;
+    (data.bio !== null) ? bio.textContent = "Bio: " + data.bio : bio.style.display = "none";
+
+    button.addEventListener("click", event =>{
+        calendarDiv.classList.toggle("calendar-hidden")
+        
+        if(button.textContent === buttonText1){
+            button.textContent = buttonText2;
+        }
+        else{
+            button.textContent = buttonText1;
+        }
+    });
 
     profile.appendChild(a);
+
+    if(useCalendar){
+        const calendar = document.createElement("div");
+        const calImg = document.createElement("img");
+        calendar.appendChild(calImg);
+        calendarDiv.appendChild(calendar);
+
+        calImg.src = `http://ghchart.rshah.org/${data.login}`;
+        calImg.style.width = "100%";
+    }
+    else{
+        calendarDiv.style.display = "none";
+    }
 
     return card;
 }
